7819a8ea9e0583a1ce297be0bcde57e78bf44698
KarolinZak <karolina.zak2006@gmail.com>
2025-12-14 10:29:38 +0200

    Added dark mode option

--- FinalProject/fronted/src/App.tsx
+++ FinalProject/fronted/src/App.tsx
@@ -1,11 +1,21 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import './App.css'
 import Navbar from "./components/Navbar/Navbar"
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 
 const App : React.FC =() => {
 
   return (
     <>
+    <ThemeProvider theme={theme}>
       <Navbar/>
+    </ThemeProvider>
+      
     </>
   )
 }
--- FinalProject/fronted/src/components/Navbar/Navbar.tsx
+++ FinalProject/fronted/src/components/Navbar/Navbar.tsx
@@ -1,29 +1,41 @@
-import { AppBar, Box, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, IconButton, colors } from '@mui/material';
 import { Menu as MenuIcon } from '@mui/icons-material';
+import { useColorScheme } from '@mui/material/styles';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 
-const Navbar : React.FC = () => {
+const Navbar: React.FC = () => {
+  const { mode, setMode } = useColorScheme();
+  if (!mode) {
+    return null;
+  }
+  
   return (
-    <Box sx={{ width: "100vw", padding: 0 }}>
-      <AppBar position="static" sx={{ backgroundColor: '#d55c5c'}}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar
+      position="fixed"
+      sx={{
+        top: 0,
+        left: 0,
+        right: 0,
+        width: '100vw',
+        backgroundColor: '#d55c5c',
+      }}
+    >
+      <Toolbar>
+        <IconButton edge="start" color="inherit" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          News
+        </Typography>
+         <Button onClick={() =>  setMode(mode === "dark" ? "light" : "dark")}>
+          {mode === "dark"? <DarkModeIcon sx={{color: '#ffffff'}}/> : <LightModeOutlinedIcon sx={{color: '#ffffff'}}/>}
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
+
 
 export default Navbar;
